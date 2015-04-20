78467c3891e682f994acdc7ec42760a6d063ef9a
Muhammad Shayan <MShayanTariq@gmail.com>
2015-04-20 19:35:23 +0500

    Added Image. Added Subs

--- lmap.py
+++ lmap.py
@@ -1,13 +1,14 @@
-#! /bin/python
+#! /bin/python3
 
 import praw
 
 r = praw.Reddit('AyyLmap by /u/m_shayan'
-		'Hosted by /u/HarmonicTimeCube')
-r.login('ayylmap', 'dankmemes')
+		'Hosted at http://www.pythonanywhere.com'
+		'https://github.com/M-Shayan/AyyLmap')
+r.login('ayylmap', 'password')
 
 
-subreddit = r.get_subreddit('androidcirclejerk+ayylmap+casualconversation+anime+amiibo+tutlegoss+m_shayan+harmonictimecube+well_played_life+el_jefe15+jpleo')
+subreddit = r.get_subreddit('redditdev+learnprogramming+programmerhumor+androidcirclejerk+ayylmap+casualconversation+amiibo+tutlegoss+m_shayan+harmonictimecub+casualc+ledootgeneration+well_played_life+el_jefe15')
 target = ["ayy", "ayy!", "Ayy", "Ayy!", "AYY", "AYY!"]
 already_done = set()
 
@@ -16,5 +17,6 @@ while True:
 	subreddit_comments = subreddit.get_comments()
 	for comment in subreddit_comments:
 		if comment.body in target and comment.id not in already_done:
-			comment.reply(' Lmap! \n \n \n \n \n *^^^I ^^^am ^^^a ^^^bot.* ^^^^/r/ayylmap ')
+			comment.reply(' [Lmap!](http://i.imgur.com/Gpg9YBR.png) \n \n \n \n \n *^^^^I ^^^^am ^^^^a ^^^^bot.* ^^^^^/r/ayylmap ')
 		already_done.add(comment.id)
+
